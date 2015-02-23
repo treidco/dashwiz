8fb2c66de24177cb8f79d93098614febde92c958
Tim Reid <treid@spotxchange.com>
2015-02-22 17:05:10 -0700

    refactored to put getTxns in a temp service class

--- src/main/scala/core/application.scala
+++ src/main/scala/core/application.scala
@@ -1,24 +1,16 @@
 package core
 
-import core.DatabaseCfg._
-
-import scala.slick.driver.H2Driver.simple._
-
-object Dashwiz extends App with BootStrap with executeTxns {
-
-  getTxns()
-
-}
+import service.executeTxns
 
 
-trait executeTxns {
+object Dashwiz extends App with BootStrap {
 
-  def getTxns(txn_id: Int = 123) = {
-    db.withSession { implicit session =>
-      tescoTable.list.filter(_.amount > 100).map(tx => tx.amount).foreach(println)
-    }
-  }
+  //shorthand with trait
+  val txns = new executeTxns {} getTxns()
 
 }
+
+
+
 
 
---
+++ src/main/scala/service/tescoservice.scala
@@ -0,0 +1,15 @@
+package service
+
+import core.DatabaseCfg._
+
+import scala.slick.driver.H2Driver.simple._
+
+trait executeTxns {
+
+  def getTxns(txn_id: Int = 123) = {
+    db.withSession { implicit session =>
+      tescoTable.list.filter(_.amount > 100).map(tx => tx.amount).foreach(println)
+    }
+  }
+
+}
